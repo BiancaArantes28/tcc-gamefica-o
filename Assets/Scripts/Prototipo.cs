c9f2a8fedff93e749d2a9ccec9a8b2b111b6495a
BiancaArantes28 <biancaarantes28@gmail.com>
2018-04-23 19:05:46 -0300

    Prototipo Scene

---
+++ Assets/Scripts/Prototipo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prototipo : MonoBehaviour {
+
+	public GameObject[] ingredientes;
+	public Sprite lanche;
+	static public int[] arrayingredientes;
+	static public int i = 0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+	public void selecionaIngrediente(){
+		print (i);
+		ingredientes[i].GetComponent<SpriteRenderer>().sprite = lanche;
+
+		ingredientes[i].SetActive(true);
+		i++;
+	}
+
+
+}
