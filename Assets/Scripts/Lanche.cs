4691d3911ccdf9777c8431015b59a5a27bfddb56
biancaarantes28 <biancaarantes28@gmail.com>
2018-05-12 20:04:24 -0300

    Random orders

--- Assets/Scripts/Lanche.cs
+++ Assets/Scripts/Lanche.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Lanche : MonoBehaviour {
 
+	static public int[] montar_lanche = new int[4];
+	int num;
 	// Use this for initialization
 	void Start () {
-		
+		montarLanche ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	public void montarLanche(){
+		for (int i = 0; i < montar_lanche.Length; i++) {
+			num = Random.Range (1, 5);
+			montar_lanche [i] = num;
+		}
+	}
+
 }
--- Assets/Scripts/Prototipo.cs
+++ Assets/Scripts/Prototipo.cs
@@ -11,24 +11,26 @@ public class Prototipo : MonoBehaviour {
 	public Sprite lanche;
 	public AudioSource musica;
 	public AudioSource musica_descartalanche;
-	// int[] arrayingredientes;
-	int[] arrayingredientes = new int[6];
+	//int[] arrayingredientes = new int[6];
+	 
+
+	int num;
 	static public int i = 0;
 	public int value;
-	int[] usuario = new int[6];
+	//int[] usuario = new int[6];
 	bool resposta;
 	public Text txt;
 	int placar = 0;
-	int verificar = 1;
+	//int verificar = 1;
 	// Use this for initialization
 	void Start () {
-		
+		montaPedido ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 
-		montaPedido ();
 		
 	}
 
@@ -38,67 +40,99 @@ public class Prototipo : MonoBehaviour {
 
 		ingredientes[i].SetActive(true);
 
-		usuario[i] = value;
-		resposta = checkLanche (usuario [i]);
+		//usuario[i] = value;
+		resposta = checkLanche ();
 		if (resposta == false) {
 			descartaLanche ();
 			musica_descartalanche.Play ();
 			i = 0;
 		} else {
-			// print (i);
-//			verificar = i + 1;
-//			// print (verificar);
-//			if (verificar == arrayingredientes.Length) {
-//				confereLancheEAumentaPlacar ();
-//			}
 			i++;
 
 		}
-		//i++;
 	}
-
 	public void montaPedido(){
-		for (int b = 0; b < pedidos.Length; b++) {
-			arrayingredientes[b] = b + 1;
-			pedidos[b].GetComponent<SpriteRenderer>().sprite = spritepedidos[b];
+		//arrayingredientes[0] = 1;
+		pedidos[0].GetComponent<SpriteRenderer>().sprite = spritepedidos[0];
+		pedidos[0].SetActive(true);
+		for (int b = 1; b < pedidos.Length - 1; b++) {
+			num = Random.Range(1,5);
+			//arrayingredientes[b] = b + 1;
+			pedidos[b].GetComponent<SpriteRenderer>().sprite = spritepedidos[num];
 			pedidos[b].SetActive(true);
+
 		}
+		//arrayingredientes[5] = 6;
+		pedidos[5].GetComponent<SpriteRenderer>().sprite = spritepedidos[5];
+		pedidos[5].SetActive(true);
 	}
 
-	public bool checkLanche(int ingredienteSelecionado){
-		if (ingredienteSelecionado == arrayingredientes[i]) {
-			
-			if (i + 1 == 6) {
-				//Instantiate (musica, new Vector3 (-7, 2, 0), Quaternion.identity);
-				musica.Play();
+
+
+	public bool checkLanche(){
+		if (lanche == pedidos [i].GetComponent<SpriteRenderer> ().sprite) {
+			if (i == 5) {
+				i = 0;
+				musica.Play ();
 				placar += 1;
 				txt.text = placar.ToString ();
 				descartaLanche ();
 				descartaPedido ();
+				montaPedido ();
+
+				return true;
+
+			}
+			return true;
+			
+		} else {
+			return false;
+		}
+		/*if (ingredienteSelecionado == arrayingredientes[i]) {
+			
+			if (i + 1 == 6) {
+				//Instantiate (musica, new Vector3 (-7, 2, 0), Quaternion.identity);
+				//print("aqui");
+				i = 0;
+				musica.Play();
+				//print ("usuario");
+				//print (usuario [i]);
+				placar += 1;
+				txt.text = placar.ToString ();
+				descartaLanche ();
+				descartaPedido ();
+
+				montaPedido ();
+
 				return true;
 
 			}
 			return true;
 		} else {
 			return false;
-		}
+		}*/
 	}
 
 	public void descartaLanche(){
 		for (int c = 0; c < ingredientes.Length; c++) {
 			ingredientes[c].SetActive(false);
 		}
+		i = 0;
 
 	}
 
 	public void descartaPedido(){
 		for (int c = 0; c < ingredientes.Length; c++) {
-			pedidos[c].SetActive(false);
-			arrayingredientes [c] = 0;
+			pedidos [c].SetActive (false);
+			//arrayingredientes [c] = 0;
+			//usuario [c] = 0;
+
 		}
+		i = 0;
+
 	}
 
-	public void confereLancheEAumentaPlacar(){
+	/*public void confereLancheEAumentaPlacar(){
 		int erro = 0;
 		for (int c = 0; c < arrayingredientes.Length; c++) {
 			
@@ -112,7 +146,7 @@ public class Prototipo : MonoBehaviour {
 			placar += 1;
 			txt.text = placar.ToString ();
 		}
-	}
+	}*/
 
 
 }
